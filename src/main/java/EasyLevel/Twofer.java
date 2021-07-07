137040f9807ddaf9e88771dfa1970f2052ec5044
EXC1Q3X <eric_c_chiu@homedepot.com>
2021-07-07 10:06:48 -0500

    (REFACTOR) Refactor TwoFer solution

--- src/main/java/EasyLevel/Twofer.java
+++ src/main/java/EasyLevel/Twofer.java
@@ -2,12 +2,9 @@ package EasyLevel;
 
 public class Twofer {
     public String twofer(String name) {
-        if(name == null) {
-            return "One for you, one for me.";
-        } else if(name == "") {
-            return "One for you, one for me.";
-        } else {
-            return String.format("One for %s, one for me.", name);
+        if(name == "" || name == null) {
+            name = "you";
         }
+        return String.format("One for %s, one for me.", name);
     }
 }
