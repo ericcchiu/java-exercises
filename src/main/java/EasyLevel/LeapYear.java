2af40f9e7dac8a7e59cee9a5c1c5ba28dc16d9e9
EXC1Q3X <eric_c_chiu@homedepot.com>
2021-07-07 09:54:50 -0500

    (FEATURE) Add LeapYear soln

--- src/main/java/EasyLevel/LeapYear.java
+++ src/main/java/EasyLevel/LeapYear.java
@@ -2,6 +2,10 @@ package EasyLevel;
 
 public class LeapYear {
     public boolean isLeapYear(int years) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        boolean isDivisibleBy4 = years % 4 == 0;
+        boolean isDivisibleBy400 = years % 400 == 0;
+        boolean isNotDivisibleBy100 = years % 100 != 0;
+
+        return isDivisibleBy4 && isNotDivisibleBy100 || isDivisibleBy400;
     }
 }
