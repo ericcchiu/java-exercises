45539babf524c7335e45e5085bce299ca58fe031
EXC1Q3X <eric_c_chiu@homedepot.com>
2021-07-13 13:39:11 -0500

    (REFACTOR) Remove port and clean up comments

--- src/main/java/Application/main.java
+++ src/main/java/Application/main.java
@@ -3,12 +3,15 @@ package Application;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @SpringBootApplication
+@Configuration
 public class main {
+
     public static void main(String[] args) {
         SpringApplication.run(main.class, args);
-        log.info("Started Java-Exercise at port: ");
+        log.info("Started Java-Exercise");
     }
 }
