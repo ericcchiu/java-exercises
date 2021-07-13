6680a86fd6147bb95e533d316914da0e98dbc027
EXC1Q3X <eric_c_chiu@homedepot.com>
2021-07-13 13:38:43 -0500

    (FEATURE) Add Darts file, challenge and test

---
+++ src/main/java/EasyLevel/Darts.java
@@ -0,0 +1,13 @@
+package EasyLevel;
+
+public class Darts {
+
+    public Darts(double x, double y) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int score() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
